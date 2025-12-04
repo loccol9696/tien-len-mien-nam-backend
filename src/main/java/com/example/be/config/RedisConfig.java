7ea782117816e4d972caaab596464caaa0be1028
loccol9696 <loccol9696@gmail.com>
2025-12-04 13:18:24 +0700

    sua redisconfig cho deploy

--- src/main/java/com/example/be/config/RedisConfig.java
+++ src/main/java/com/example/be/config/RedisConfig.java
@@ -7,11 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.net.URI;
+import java.time.Duration;
 
 @Configuration
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -28,18 +30,24 @@ public class RedisConfig {
         config.setHostName(uri.getHost());
         config.setPort(uri.getPort());
         config.setPassword(RedisPassword.of(uri.getUserInfo().split(":", 2)[1]));
+        config.setDatabase(0);
 
-        return new LettuceConnectionFactory(config);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()
+                .and()
+                .commandTimeout(Duration.ofSeconds(5))
+                .shutdownTimeout(Duration.ofSeconds(3))
+                .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
     }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
-
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
-
         return template;
     }
 }
